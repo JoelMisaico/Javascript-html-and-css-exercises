b6bbe46f11485f68e235f0dbc75f43e6568935f8
JoelMisaico <126191246+JoelMisaico@users.noreply.github.com>
2023-03-16 13:53:03 -0500

    Add files via upload

---
+++ 11-parallax/script.js
@@ -0,0 +1,5 @@
+var scene = document.getElementById("scene");
+var text = document.getElementById("text");
+let parallaxInstanceScene = new Parallax (scene);
+let parallaxInstanceText = new Parallax (text);
+
